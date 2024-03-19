5e3bcc1ed96dc445b26b12aa815cca818620db14
IvarY <art.yurchenko.work@gmail.com>
2024-03-19 22:55:35 +0200

    added input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,24 @@
+def read_console():
+    pass
+    # return input()
+
+
+def read_file(file_path):
+    pass
+    # with open(file_path, 'r') as file:
+    #     return file.read()
+
+
+def read_file_pandas(file_path):
+    pass
+    # file_extension = file_path.split('.')[-1]
+    #
+    # if file_extension == 'csv':
+    #     data = pd.read_csv(file_path)
+    # elif file_extension == 'json':
+    #     data = pd.read_json(file_path)
+    # else:
+    #     raise ValueError("Unsupported file format. Only CSV and JSON files are supported.")
+    #
+    # return data
+
---
+++ app/io/output.py
@@ -0,0 +1,21 @@
+def write_console(output):
+    pass
+    # print(output)
+
+
+def write_file(file_path, output):
+    pass
+    # with open(file_path, 'w') as file:
+    #     file.write(output)
+
+
+def write_file_pandas(file_path, output):
+    pass
+    # file_extension = file_path.split('.')[-1]
+    #
+    # if file_extension == 'csv':
+    #     output.to_csv(file_path, index=False)
+    # elif file_extension == 'json':
+    #     output.to_json(file_path)
+    # else:
+    #     raise ValueError("Unsupported file format. Only CSV and JSON files are supported.")
